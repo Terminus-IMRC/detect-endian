87a6455850e9bc7fcf8811e5d98f2585595ffac7
Terminus <i.can.speak.c.and.basic@gmail.com>
2014-02-02 06:10:36 +0000

    initial commit

---
+++ detect-endian.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <string.h>
+#include <endian.h>
+
+int main(int argc, char *argv[])
+{
+	char str[0xff]="";
+
+	switch(__BYTE_ORDER){
+		case __LITTLE_ENDIAN:
+			strcpy(str, "Little endian");
+			break;
+		case __BIG_ENDIAN:
+			strcpy(str, "Big endian");
+			break;
+		case __PDP_ENDIAN:
+			strcpy(str, "PDP endian");
+			break;
+		default:
+			strcpy(str, "Unknown endian");
+			break;
+	}
+
+	printf("%s\n", str);
+
+	return 0;
+}
